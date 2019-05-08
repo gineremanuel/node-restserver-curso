267377f84e3313471a7baf6e47dedba215eb8eac
Emanuel <emanuelginer77@gmail.com>
2019-05-08 00:37:40 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,28 @@
 //========================
 
 process.env.PORT = process.env.PORT || 3000
+
+
+
+//========================
+//  Entorno 
+//========================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+//mongodb+srv://emanuelG:<icoscla>@cafe-zck1e.mongodb.net/test?retryWrites=true
+
+//========================
+//  Base de datos 
+//========================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+
+    urlDB = 'mongodb+srv://emanuelG:icoscla@cafe-zck1e.mongodb.net/test?retryWrites=true'
+}
+
+process.env.URLDB = urlDB;
